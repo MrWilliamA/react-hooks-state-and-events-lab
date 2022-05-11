61218d0fa70ce5c197c7bc34b9c314d3cdcfbe8a
MrWilliamA <will.archer@hotmail.com>
2022-05-11 16:58:29 +1000

    fin

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -4,21 +4,20 @@ import Item from "./Item";
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
 
-  function filtered(event) {
-    const selected = event.target.value;
-    if (selected == "Produce") {
-      setSelectedCategory((selectedCategory = "Produce"));
-    } else if (selected == "Dairy") {
-      setSelectedCategory((selectedCategory = "Dairy"));
-    } else if (selected == "Dessert") {
-      setSelectedCategory((selectedCategory = "Dessert"));
-    }
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
   }
+
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") return true;
+
+    return item.category === selectedCategory;
+  });
 
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter" onChange={filtered}>
+        <select name="filter" onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -26,7 +25,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
